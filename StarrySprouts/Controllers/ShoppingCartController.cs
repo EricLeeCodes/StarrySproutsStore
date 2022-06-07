d63552c06cb45163670918630e5ddb12b79c0c5e
Eric Lee <87954849+EricLeeCodes@users.noreply.github.com>
2022-06-07 17:03:12 -0500

    Added the Adding and Removing items from cart action

--- StarrySprouts/Controllers/ShoppingCartController.cs
+++ StarrySprouts/Controllers/ShoppingCartController.cs
@@ -27,5 +27,34 @@ namespace StarrySprouts.Controllers
 
             return View(shoppingCartViewModel);
         }
+
+        public RedirectToActionResult AddToShoppingCart(int productId)
+        {
+            //Finding the product's ID
+            var selectedProduct = _productRepository.GetAllProducts.FirstOrDefault(p => p.ProductId == productId);
+
+            //If the product is not null then we can add it to the cart.
+            if (selectedProduct != null && selectedProduct.IsInStock == true)
+            {
+                _shoppingCart.AddToCart(selectedProduct, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int productId)
+        {
+            //Finding the product's ID
+            var selectedProduct = _productRepository.GetAllProducts.FirstOrDefault(p => p.ProductId == productId);
+
+            //If the product is not null and is in stock then we can add it to the cart.
+            if (selectedProduct != null && selectedProduct.IsInStock == true)
+            {
+                _shoppingCart.RemoveFromCart(selectedProduct);
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
